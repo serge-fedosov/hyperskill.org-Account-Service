457820f6799e3c164d7744b2994dc318901bb8af
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-08-24 12:37:21 +0400

    set up build.gradle and application.properties

--- Account Service/task/src/account/AccountServiceController.java
+++ Account Service/task/src/account/AccountServiceController.java
@@ -1,6 +1,7 @@
 package account;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,11 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class AccountServiceController {
+
+    @GetMapping("/api/empl/payment/")
+    public String testAuth() {
+        return "authenticated is accessed";
+    }
 
     @PostMapping("/api/auth/signup")
     public BaseUser postUser(@RequestBody User user) {
